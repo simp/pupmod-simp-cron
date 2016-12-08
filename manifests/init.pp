7c6f39ccc968193c203764ee672f2ce201156966
Nick Miller <nick.miller@onyxpoint.com>
2016-12-08 15:42:51 -0500

    (SIMP-2232) Create cron module
    Material used to be in simplib.
    
    SIMP-2232 #close
    SIMP-1679 #comment created cron module

---
+++ manifests/init.pp
@@ -0,0 +1,47 @@
+# This class manages /etc/cron.allow and /etc/cron.deny and the
+# crond service.
+#
+# @param users
+#   An array of additional cron users, using the defiend type cron::user.
+#
+class cron (
+  Boolean $install_tmpwatch,
+  Array[String] $users
+) {
+
+  $users.each |String $user| {
+    ::cron::user { $user: }
+  }
+  ::cron::user { 'root': }
+
+  simpcat_build { 'cron':
+    order            => ['*.user'],
+    clean_whitespace => 'leading',
+    target           => '/etc/cron.allow'
+  }
+
+  file { '/etc/cron.allow':
+    ensure    => 'present',
+    owner     => 'root',
+    group     => 'root',
+    mode      => '0600',
+    audit     => 'content',
+    subscribe => Simpcat_build['cron']
+  }
+
+  file { '/etc/cron.deny':
+    ensure => 'absent'
+  }
+
+  # CCE-27070-2
+  service { 'crond':
+    ensure     => 'running',
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true
+  }
+
+  if $install_tmpwatch {
+    package { 'tmpwatch': ensure => latest }
+  }
+}
---
+++ manifests/user.pp
@@ -0,0 +1,20 @@
+# Add the user $name to /etc/cron.allow
+#
+# @param name
+#   The user to add to /etc/cron.allow
+#
+define cron::user {
+  include '::cron'
+
+  $l_name = regsubst($name,'/','__')
+
+  simpcat_fragment { "cron+${l_name}.user":
+    content =>  "${name}\n"
+  }
+
+  pam::access::manage { "cron_user_${l_name}":
+    users   => $l_name,
+    origins => ['cron', 'crond']
+  }
+
+}
