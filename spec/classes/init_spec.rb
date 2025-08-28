dfe63c3a5c49a5a902ca7c25ab2132c7d9757b44
Steven Pritchard <steve@sicura.us>
2025-08-28 03:33:25 -0500

    Clean up for rubocop (#70)
    * Clean up for rubocop
    * Bump minimum required version of simp-rake-helpers
    * Bump required simp-beaker-helpers version

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -11,40 +11,48 @@ describe 'cron' do
         context 'with default parameters' do
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_class('cron') }
-          it { is_expected.to contain_class('cron::install')}
-          it { is_expected.to contain_class('cron::service')}
+          it { is_expected.to contain_class('cron::install') }
+          it { is_expected.to contain_class('cron::service') }
           it { is_expected.to create_cron__user('root') }
           it { is_expected.to create_concat('/etc/cron.allow') }
-          it { is_expected.to create_file('/etc/cron.deny').with({:ensure => 'absent'}) }
-          #defaults for install
+          it { is_expected.to create_file('/etc/cron.deny').with({ ensure: 'absent' }) }
+          # defaults for install
           it { is_expected.to create_package('cronie') }
           it { is_expected.not_to create_package('tmpwatch') }
         end
 
         context 'with a users parameter' do
-          let(:params) {{
-            :users => ['test','foo','bar']
-          }}
+          let(:params) do
+            {
+              users: ['test', 'foo', 'bar']
+            }
+          end
+
           it { is_expected.to create_cron__user('test') }
           it { is_expected.to create_cron__user('foo') }
           it { is_expected.to create_cron__user('bar') }
         end
 
         context 'when not managing packages' do
-          let(:params) {{
-            :install_tmpwatch => true,
-            :manage_packages  => false
-          }}
+          let(:params) do
+            {
+              install_tmpwatch: true,
+           manage_packages: false
+            }
+          end
 
-          it { is_expected.to_not create_package('tmpwatch') }
-          it { is_expected.to_not create_package('cronie') }
+          it { is_expected.not_to create_package('tmpwatch') }
+          it { is_expected.not_to create_package('cronie') }
         end
 
         context 'when add_root_user is false' do
-          let(:params) {{
-            :add_root_user => false
-          }}
-          it { is_expected.to_not create_cron__user('root') }
+          let(:params) do
+            {
+              add_root_user: false
+            }
+          end
+
+          it { is_expected.not_to create_cron__user('root') }
         end
       end
     end
